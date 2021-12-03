2fe98b6cd22e0df7a65552f0a590b4713136a437
Paul <pavel.k.313@mail.ru>
2021-12-03 17:32:52 +0300

    5 series case

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def prob_equals(n, p):
 
 
 # probability of n successes
-def prob_success(n, p):
+def prob_success(n, p, s, nn):
     return pow(p, n)
 
 
@@ -45,12 +45,13 @@ def prob_no_success(n, p):
 
 
 # probability of less than n successes
-def prob_less(n, p):
+def prob_less(n, p, s, nn):
     sp = 0
     # for i in range(n - 1):
     # sp += pow(p, i)
     count = n
     t = 1
+    sp += pow(1 - p, n)
     while count - t > 0:
         sp += prob_equals(count - t, p) * prob_no_success(t, p)
         t += 1
@@ -60,9 +61,46 @@ def prob_less(n, p):
     return sp
 
 
+def success(n, p):
+    return pow(p, n)
+
+
+def less(n, p):
+    s = pow(1 - p, n)
+    c = n - 1
+    while c > 0:
+        s += pow(p, c) * pow(1 - p, n - c)
+        c -= 1
+    return s
+
+
 # probability of team A winning 5 penalty series
-def prob_win_a(pa, sa, pb, sb, n):
+def prob_win_a(pa, pb, sa, sb, n):
+    first_series = 5
+    nn = n
+    p = 0
+    while first_series - sa > 0 or first_series - sb > 0:
+        # p += prob_success(first_series, pa, sa, n) * prob_less(first_series, pb, sb, n)
+        p += success(first_series, pa) * less(first_series, pb)
+        first_series -= 1
+        nn += 1
+
+    p = (pow(0.65, 5) * (pow(0.55, 5) + 0.45 * pow(0.55, 4) + pow(0.45, 2) * pow(0.55, 3) + pow(0.45, 3) * pow(0.55, 2) +
+                     pow(0.45, 4) * pow(0.55, 1))) + (pow(0.65, 4) * (pow(0.55, 5) + 0.45 * pow(0.55, 4) + pow(0.45, 2)
+                                                                      * pow(0.55, 3) + pow(0.45, 3) * pow(0.55, 2))) + \
+    (pow(0.65, 3) * (pow(0.55, 5) + 0.45 * pow(0.55, 4) + pow(0.45, 2) * pow(0.55, 3))) + \
+    (pow(0.65, 2) * (pow(0.55, 5) + 0.45 * pow(0.55, 4))) + (0.65 * (pow(0.55, 5)))
+    '''
     p = prob_success(5, pa) * prob_less(5, pb) + prob_success(4, pa) * prob_less(4, pb) + \
         prob_success(3, pa) * prob_less(3, pb) + prob_success(2, pa) * prob_less(2, pb) + \
         prob_success(1, pa) * prob_no_success(5, pb)
+    '''
     print(p)
+
+
+Pa = 0.65
+Pb = 0.45
+Sa = 0
+Sb = 0
+N = 0
+prob_win_a(Pa, Pb, Sa, Sb, N)
